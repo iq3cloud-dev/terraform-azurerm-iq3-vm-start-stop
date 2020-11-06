b24f0a353db6481d33d773024170582ff09907f7
iq3cloud <github@iq3cloud.com>
2020-11-06 14:01:02 +0100

    Updated repo by Pipeline

---
+++ main.tf
@@ -0,0 +1,55 @@
+# A Terraform module to create a subset of cloud components
+# Copyright (C) 2020 IQ3 CLOUD Solutions Direkt GmbH
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version. 
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For questions and contributions please contact info@iq3cloud.com
+# https://github.com/iq3cloud-dev/terraform-azurerm-iq3-vnet
+
+locals {
+  parameters_body = {
+    actionType = {
+      value = var.action_type
+    }
+    vmName = {
+      value = var.vm_name
+    }
+    clientId = {
+      value = var.sp_client_id
+    }
+    clientSecret = {
+      value = var.sp_client_secret
+    }
+    runHours = {
+      value = var.hours
+    }
+    runMinutes = {
+      value = var.minutes
+    }
+    runWeekDays = {
+      value = var.week_days
+    }
+    timeZone = {
+      value = var.time_zone
+    }
+  }
+}
+
+resource "azurerm_resource_group_template_deployment" "logicapp" {
+  name                = "${var.action_type}-deployment"
+  resource_group_name = var.resource_group_name
+  deployment_mode     = "Incremental"
+  parameters_body     = jsonencode(local.parameters_body)
+  template_body       = file("${path.module}/main.json")
+}
---
+++ variables.tf
@@ -0,0 +1,67 @@
+﻿# A Terraform module to create a subset of cloud components
+# Copyright (C) 2020 IQ3 CLOUD Solutions Direkt GmbH
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version. 
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For questions and contributions please contact info@iq3cloud.com
+# https://github.com/iq3cloud-dev/terraform-azurerm-iq3-vnet
+
+variable "resource_group_name" {
+  type = string
+  description = "name of resorcegroup. Should be the same as the parrent virtual machine"
+}
+
+variable "action_type" {
+  type        = string
+  description = "The Type of action. must be 'StartVm' or 'StopVm'."
+}
+
+variable "vm_name" {
+  type        = string
+  description = "The name of the VM to be stopped or started."
+}
+
+variable "sp_client_id" {
+  type        = string
+  description = "Service principal clientID."
+}
+
+variable "sp_client_secret" {
+  type        = string
+  description = "Service principal secret value."
+}
+
+variable "hours" {
+  type        = list(string)
+  default = ["12"]
+  description = "Hour when the scheduler should start."
+}
+
+variable "minutes" {
+  type        = list(number)
+  default = [0]
+  description = "Minute of hour when the scheduler should start."
+}
+
+variable "week_days" {
+  type        = list(string)
+  default = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+  description = "week days when the scheduler should be executed"
+}
+
+variable "time_zone" {
+  type        = string
+  default = "W. Europe Standard Time"
+  description = "Time zone. default is 'W. Europe Standard Time'."
+}
